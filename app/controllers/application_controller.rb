5d3225fe6526becfd5c680fae00f78335311a56c
SolomonRFeldman <SolomonRFeldman@gmail.com>
2021-09-26 09:43:58 -0400

    replaces before action with rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::API
-  before_action :fetch_index_results, only: %i[index]
-
-  def index
-    render json: @results, status: 200
+  rescue_from SocialMediaApiService::BadResponseError do |error|
+    render(
+      json: { 
+        error: { 
+          api_name: error.api_name, 
+          message: error.message, 
+          type: 'invalid response'
+        } 
+      }, 
+      status: 500
+    )
   end
 
-  private
-
-  def fetch_index_results
-    @results = SocialMediaApiService.index
-  rescue SocialMediaApiService::BadResponseError => error
-    render json: { error: { api_name: error.api_name, message: error.message, type: 'invalid response'} }, status: 500
+  def index
+    render json: SocialMediaApiService.index, status: 200
   end
 end
